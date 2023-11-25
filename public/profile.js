a14a553d84e76ba4cc82b69f7099641aeed20216
Estebom <estebomofficial@gmail.com>
2023-11-25 18:45:24 -0500

    added edit pofile

--- public/profile.js
+++ public/profile.js
@@ -1,35 +1,68 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Now you can safely add event listeners to your elements.
+    let newProfilePictureDataUrl = null; // Temporary storage for the new profile picture
 
-    document.getElementById('userName').addEventListener('input', function() {
-        localStorage.setItem('userName', this.value);
+    // Function to save all profile information in JSON format
+    function saveProfileInfo() {
+        const profileData = {
+            userName: document.getElementById('userNameDisplay').value,
+            userDescription: document.getElementById('profileDescriptionDisplay').value,
+            profilePicture: newProfilePictureDataUrl || document.getElementById('profilePictureDisplay').src
+        };
+
+        // Save the profile data to localStorage
+        localStorage.setItem('profileData', JSON.stringify(profileData));
+        alert('Profile information saved successfully!');
+        
+        // Clear the temporary image data URL after saving
+        newProfilePictureDataUrl = null;
+    }
+
+    // Toggle read-only state of the profile fields and show/hide the file input
+    function toggleEditProfile(editing) {
+        document.getElementById('userNameDisplay').readOnly = !editing;
+        document.getElementById('profileDescriptionDisplay').readOnly = !editing;
+        // Always display the profile picture
+        document.getElementById('profilePictureDisplay').style.display = 'inline-block';
+        // Toggle the file input for profile picture
+        document.getElementById('profilePictureInput').style.display = editing ? 'inline-block' : 'none';
+        
+        // Toggle the Save and Edit buttons
+        document.getElementById('saveProfileBtn').style.display = editing ? 'inline-block' : 'none';
+        document.getElementById('editProfileBtn').style.display = editing ? 'none' : 'inline-block';
+    }
+
+    // Load stored profile information on page load
+    function loadProfile() {
+        const savedProfileData = JSON.parse(localStorage.getItem('profileData'));
+        if (savedProfileData) {
+            document.getElementById('userNameDisplay').value = savedProfileData.userName || '';
+            document.getElementById('profileDescriptionDisplay').value = savedProfileData.userDescription || '';
+            document.getElementById('profilePictureDisplay').src = savedProfileData.profilePicture || '';
+        }
+    }
+
+    // Initialize the profile data on page load
+    loadProfile();
+
+    // Event listener for the Edit Profile button
+    document.getElementById('editProfileBtn').addEventListener('click', function() {
+        toggleEditProfile(true);
     });
 
-    document.getElementById('profilePicture').addEventListener('change', function() {
+    // Event listener for the Save Profile button
+    document.getElementById('saveProfileBtn').addEventListener('click', function() {
+        saveProfileInfo();
+        toggleEditProfile(false);
+    });
+
+    // Event listener for profile picture changes
+    document.getElementById('profilePictureInput').addEventListener('change', function() {
         const reader = new FileReader();
         reader.onload = function(e) {
-            document.getElementById('displayPicture').src = e.target.result;
+            // Update the display picture src with the selected file's data URL
+            newProfilePictureDataUrl = e.target.result; // Store the new data URL temporarily
+            document.getElementById('profilePictureDisplay').src = newProfilePictureDataUrl;
         };
         reader.readAsDataURL(this.files[0]);
     });
-
-    document.getElementById('profileDescription').addEventListener('input', function() {
-        localStorage.setItem('userDescription', this.value);
-    });
-
-    // When the window is loaded, update the input fields and image based on stored values.
-    window.onload = function() {
-        // This will set the value to either the stored value or an empty string if there's nothing stored.
-        document.getElementById('userName').value = localStorage.getItem('userName') || '';
-        document.getElementById('profileDescription').value = localStorage.getItem('userDescription') || '';
-        
-        // If there's an image stored, update the `src` of the `displayPicture` element.
-        // Assuming the image is stored in localStorage in Base64 format.
-        const storedImage = localStorage.getItem('profilePicture');
-        if (storedImage) {
-            document.getElementById('displayPicture').src = storedImage;
-        }
-
-        // Here you can also add code to display the reviews if necessary.
-    };
 });
